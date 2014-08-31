6e3bbcc3e501821375b0b19345a8c72e21ecda1f
stett <stettjawa@gmail.com>
2014-08-31 12:37:11 -0700

    All parent/child tests passing

--- src/frame_test/tests/test_core.h
+++ src/frame_test/tests/test_core.h
@@ -41,8 +41,7 @@ namespace {
         EXPECT_EQ(children->size(), (unsigned)2) << "The parent's child list should be exactly 2 items long";
         EXPECT_NE(children->find(e_child_0), children->end()) << "The first child we added isn't showing up in the parent's child list";
         EXPECT_NE(children->find(e_child_1), children->end()) << "The second child we added isn't showing up in the parent's child list";
-        //Entity* e_child_0_parent = e_child_0->get_component<Parent>()->get()
-        //EXPECT_EQ(e_child_0_parent, e_parent) << "The first child we added doesn't point to the parent";
-        //EXPECT_EQ(*(e_child_1->get_component<Parent>()), e_parent) << "The second child we added doesn't point to the parent";
+        EXPECT_EQ(e_child_0->get_component<Parent>()->get(), e_parent) << "The first child we added doesn't point to the parent";
+        EXPECT_EQ(e_child_1->get_component<Parent>()->get(), e_parent) << "The second child we added doesn't point to the parent";
     }
 }
