4fa28152a6b69a94c4f28db1708644c79065a19d
stett <stettjawa@gmail.com>
2014-11-12 16:06:57 -0800

    b2d and prototypes for physics stuff

--- src/frame_sandbox/entity_factories.hpp
+++ src/frame_sandbox/entity_factories.hpp
@@ -8,6 +8,8 @@
 #include "frame/core/Parent.h"
 #include "frame/core/Children.h"
 #include "components/Slot.hpp"
+#include "components/PhysicsBody.hpp"
+#include "components/PhysicsWorld.hpp"
 using frame::Frame;
 using frame::Entity;
 using frame::Parent;
@@ -17,16 +19,17 @@ Entity* box_entity(Frame* f, Entity* parent = 0, int slot_x = 0, int slot_y = 0)
 
     // Create a root metabox
     auto e = f->add_entity<
-        Children
+        Children,
         //Slots,
         //BoxDoors,
-        //PhysicsWorld
+        PhysicsWorld
     >();
 
     // If parent is set, add necessary extra parameters
     if (parent) {
         e->add_component<Parent>()->set(parent);
         e->add_component<Slot>()->set(slot_x, slot_y);
+        e->add_component<PhysicsBody>();
         //e->add_component(new PhysicsBody(
         //    parent->get_component<PhysicsWorld>));
     }
