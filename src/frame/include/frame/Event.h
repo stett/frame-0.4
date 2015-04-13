0f656c1ca8c2550ca7a7fb46bb60df7e71c4b6cc
stett <stettjawa@gmail.com>
2015-04-12 22:02:00 -0700

    deleted some ugly old code rags

--- src/frame/include/frame/Event.h
+++ src/frame/include/frame/Event.h
@@ -61,27 +61,4 @@ namespace frame {
                 listener->trigger(args...);
         }
     };
-
-
-
-    /*
-    // Abstract base event structure
-    struct Event { virtual ~Event() {} };
-
-    // Abstract event handler interface
-    class EventHandlerBase {
-     public:
-        virtual ~EventHandlerBase() {}
-    };
-
-    // Abstract event handler interface template
-    template <typename T>
-    class EventHandler : public EventHandlerBase {
-     public:
-        virtual ~EventHandler() {}
-
-     protected:
-        virtual void handle(T* e) = 0;
-    };
-    */
 };
